344976d2e7b48310c52313aafba56c0e58e84d28
nandikajain <jainnandika@gmail.com>
2020-07-07 20:53:32 +0530

    Layout and multiple dynamic cards using React

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  var date = new Date();
+  const year = date.getFullYear();
+  return (
+    <footer>
+      <p> Copyright ⓒ {year}</p>
+    </footer>
+  );
+}
+export default Footer;
